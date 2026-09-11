931cb82b1a36e02ad755f042c7da8ef4a833f519
XSOP <china.xsir@gmail.com>
2026-09-11 17:18:16 +0800

    Delete update-server directory

--- update-server/schema.sql
+++
@@ -1,7 +0,0 @@
--- ===== 系统配置表 =====
-CREATE TABLE IF NOT EXISTS config (
-  key TEXT PRIMARY KEY,
-  value TEXT NOT NULL
-);
--- 插入初始密码
-INSERT OR IGNORE INTO config (key, value) VALUES ('admin_password', 'frapi-admin-2026');
